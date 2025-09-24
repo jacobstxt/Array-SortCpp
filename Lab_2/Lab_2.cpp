8d67a7cd29185dc91da224aafab25a68ac47ae0b
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-09-24 11:59:22 +0300

    second commit

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -1,19 +1,16 @@
-﻿// Lab_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include "Sort.h"
 
 int main()
 {
     const int n = 30;
     int A[n] = { 
-      14, 27, 6, 25, 1,
-      21, 10, 28, 4, 13,
-      17, 8, 24, 2, 30,
-      9, 19, 23, 11, 29,
-      7, 22, 5, 20, 16,
-      15, 26, 3, 12, 18
+      14, 27, 6,  25, 1,
+      21, 10, 28, 4,  13,
+      17, 8,  24, 2,  30,
+      9,  19, 23, 11, 29,
+      7,  22, 5,  20, 16,
+      15, 26, 3,  12, 18
     };
  
 
@@ -39,14 +36,3 @@ int main()
     show(A, n);
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Lab_2/Sort.h
+++ Lab_2/Sort.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 void show(int* A, int n);
 void my_swap(int& a, int& b);
 
